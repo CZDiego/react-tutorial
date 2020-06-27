62cb068a579ebf534bf48292a96eb8acb18847c1
Diego Contreras <diego.contreras@base22.com>
2020-06-27 16:20:00 -0500

    Add state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import './App.css';
-import {Greet} from './components/Greet';
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
       {/* Props are immutable */}
-      <Greet name="Bruce" heroName="Batman">
+      {/*<Greet name="Bruce" heroName="Batman">
         <p>This is children #1</p>
         <p>This is children #2</p>
         <p>This is children #3</p>
-      </Greet>
-      <Greet name="Clark" heroName="Super man">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wonder Woman"/>
+      </Greet>*/}
 
-      <Welcome name="Bruce" heroName={"Batman"}/>
-      <Welcome name="Clark" heroName={"Super man"}/>
-      <Welcome name="Diana" heroName={"Wonder Woman"}/>
+      <Message/>
 
-      <Hello/>
     </div>
   );
 }
