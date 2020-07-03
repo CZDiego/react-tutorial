f0db95b353f824de6e8d9f1ed9891290e3a5a984
Diego Contreras <diego.contreras@base22.com>
2020-07-03 13:38:12 -0500

    Destructuring objects (props & state)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Welcome extends Component {
 
   render() {
-    return <h1>Welcome {this.props.heroName} a.k.a {this.props.name}</h1>;
+    const {name, heroName} = this.props;
+    //const {state1, state2} = this.state;
+
+    return <h1>Welcome {heroName} a.k.a {name}</h1>;
   }
 
 }
