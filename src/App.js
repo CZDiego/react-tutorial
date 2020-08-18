546ca197094575662e204dca26cd9203d12c3cb6
Diego Contreras <diego.contreras@base22.com>
2020-08-18 10:06:08 -0500

    Add Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import PortalDemo from "./components/portal/PortalDemo";
+import Hero from "./components/errorBoundary/Hero";
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 
 function App() {
+
+  const heros = [
+    "Batman",
+    "SuperMan",
+    "Joker"
+  ];
   return (
     <div className="App">
-      <PortalDemo/>
+
+      {heros.map((hero, index) => {
+        return <ErrorBoundary key={index}><Hero heroName={hero}/></ErrorBoundary>
+      })}
+
     </div>
   );
 }
